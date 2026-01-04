4a07974f4d162369689ebd01999168dc709fdd42
davcarbajal32 <davcarbajal32@gmail.com>
2026-01-04 15:21:41 -0800

    Add files via upload

---
+++ cleandata.py
@@ -0,0 +1,81 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+#going to simplfiy the used_cars.csv file into these columns:
+#brand (encoded), model, model_year, mileage, fuel_type, transmission_type, clean_title, price
+
+df = pd.read_csv('used_cars.csv')
+
+#strip whitesapce from the dataset
+for c in df.select_dtypes(include="object").columns:
+    df[c] = df[c].str.strip()
+
+#function for reading/simplifing the transmission
+def simply_transmission(s):
+    if not isinstance(s, str):
+        return None
+    s = s.lower()
+    if "manual" in s or "m/t" in s:
+        return "Manual"
+    elif "cvt" in s:
+        return "CVT"
+    elif 'automatic' in s or "a/t" in s:
+        return "A/T"
+    else:
+        return "Other"
+
+#removing unnecessary symbols such as "mi", "$", etc.
+mileage = df['mileage'].str.replace('[, mi]','', regex=True).astype(float)
+price = df['price'].str.replace('[, $]','', regex=True).astype(float)
+
+#storing the new transmission types
+transmission_type = df['transmission'].apply(simply_transmission)
+
+#getting model
+model = df['model']
+
+#getting_model years
+model_year = df['model_year']
+
+#getting fuel_type
+fuel_type = df['fuel_type'].fillna('Other').replace('-', 'Other')
+
+#normalizing the fuel_type
+bad_tokens = ['-', '–', '—', '', 'nan', 'None', 'not supported']
+#replacing the bad tokens with 'other'
+fuel_type = (
+    df['fuel_type'].astype('string').str.strip().replace(bad_tokens, pd.NA).fillna('Other'))
+
+#tunring clean_title into true(1) or false(1) instead of Yes and No 
+clean_title = df['clean_title'].fillna('No').str.strip().str.title().map({"Yes": 1, "No": 1})
+
+#encoding the models
+encoder = LabelEncoder()
+brands_encoded = encoder.fit_transform(df['brand'].fillna("Unknown"))
+
+# combine them into a new DataFrame
+data = {
+    'brand': brands_encoded,
+    'model': model,
+    'model_year': model_year,
+    'mileage': mileage,
+    'fuel_type': fuel_type,
+    'transmission_type': transmission_type,
+    'clean_title': clean_title,
+    'price': price
+}
+
+df_new = pd.DataFrame(data)
+
+# save to a new CSV file
+df_new.to_csv('clean_used_cars.csv', index=False)
+print("New simplified Databased saved as: 'used_cars_database.csv'")
+
+#saving the brand_mapping into a text file to see brand encoding being used
+brand_map = dict(zip(encoder.classes_, encoder.transform(encoder.classes_)))
+with open('brand_map.txt','w') as file:
+    for brand, code in brand_map.items():
+        file.write(f'{brand}: {code}\n')
+print("Successfully created 'brand_map.txt'.")
+
+
+
